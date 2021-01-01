61f7ed99a79004ffc7311f1d0e9cbbadfb6235a0
AkshayaPar <74913856+AkshayaPar@users.noreply.github.com>
2021-01-01 12:48:06 +0530

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -1,24 +1,3 @@
-/*class Dustbin{
-    constructor(x, y){
-        
-        var options = {
-            isStatic: true
-        }
-        this.body = Bodies.rectangle(x, y, width, height, options);
-        this.width = width;
-        this.height = height;
-        World.add(world, this.body);
-    }
-    display(){
-        var dustbinpos = this.body.position;
-        translate(dustbinpos.x, dustbinpos.y);
-        rectMode(CENTER);
-        rect(this.body.position.x,this.body.position.y,this.width,this.height);
-        fill("green");
-       
-    }
-}*/
-
 class Dustbin {
     constructor(x, y, width, height) {
       this.body = Bodies.rectangle(x, y, width, height,{isStatic:true});
@@ -36,9 +15,9 @@ class Dustbin {
       translate(pos.x, pos.y);
       rotate(angle);
       rectMode(CENTER);
-      fill("white");
-      stroke("white");
-      strokeWeight(4)
+      //fill("white");
+      //stroke("white");
+      //strokeWeight(4)
       this.dustbin.scale=0.5
       rect(0, 0, this.width, this.height);
       pop();
